1faa7042fa7ef0d50e1dcbe6701199d1fa3793a0
BiswasSwagatam <swagatam.04.biswas@gmail.com>
2024-11-08 12:42:35 +0530

    done

---
+++ next.config.js
@@ -0,0 +1,19 @@
+// import type { NextConfig } from "next";
+
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ["links.papareact.com", "cloud.appwrite.io"],
+//   }
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+      domains: ["cloud.appwrite.io", "links.papareact.com"]
+  }
+}
+
+module.exports = nextConfig
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["links.papareact.com", "cloud.appwrite.io"],
-  }
-};
-
-export default nextConfig;
